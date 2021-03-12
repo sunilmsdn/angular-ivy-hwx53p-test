9c7bced7b3fae3ea6ab0616e395ffe8dd07ad99e
sunilmsdn <sunil.msdn@gmail.com>
2021-03-12 18:28:57 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component, VERSION } from '@angular/core';
-
+import {Employee} from './emp';
 @Component({
   selector: 'my-app',
- template:`<h1> Hi {{name}} <br/> {{num}}</h1>
-  <input type="text" [value]='num'/>`
+ template:`<h1> Hi {{name}} <br/> {{num}} </h1> <br/>
+  <li *ngFor='let e of emp' >{{e.name}}</li>`
 })
 export class AppComponent  {
   name:string="sunil";
   num:Number=30;
+
+  emp:Employee[]=[{
+
+    name:"Sunil",
+    id:1
+  },
+   {
+
+    name:"Anu",
+    id:2
+  },
+  {
+
+    name:"Saanvi",
+    id:3
+  }
+  ]
 }
